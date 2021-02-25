c2b8b6cb34f3a9ac78008c807cdb8aa67d84a874
Simon Anderson <3278712+thelegendofando@users.noreply.github.com>
2021-02-25 13:31:23 +0000

    split tests

---
+++ FunctionDotNet.Tests/Lift1.cs
@@ -0,0 +1,29 @@
+﻿using FunctionalDotNet.Tests.Helpers;
+using NUnit.Framework;
+
+namespace FunctionalDotNet.Tests
+{
+    [TestFixture]
+    public class Lift1
+    {
+        [Test]
+        public void CanMapWithAnotherFunction()
+        {
+            var addOne = Result.Lift<int, int>(Calculator.AddOne);
+            var addTwo = addOne.Map(Calculator.AddOne);
+            var result = addTwo(Result.Success(0)).ItemOrDefault;
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void CanBindWithAnotherFunction()
+        {
+            var addOne = Result.Lift<int, int>(SafeCalculator.AddOne);
+            var addTwo = addOne.Bind(SafeCalculator.AddOne);
+            var result = addTwo(Result.Success(0)).ItemOrDefault;
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ FunctionDotNet.Tests/Lift2.cs
@@ -0,0 +1,35 @@
+﻿using FunctionalDotNet.Tests.Helpers;
+using NUnit.Framework;
+
+namespace FunctionalDotNet.Tests
+{
+    [TestFixture]
+    public class Lift2
+    {
+        [Test]
+        public void CanMapWithAnotherFunction()
+        {
+            var addTwo = Result
+                .Lift<int, int, int>(Calculator.Add)
+                .Map(Calculator.AddOne)
+                .Apply(Result.Success(1));
+
+            var result = addTwo(Result.Success(1)).ItemOrDefault;
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void CanBindWithAnotherFunction()
+        {
+            var addTwo = Result
+                .Lift<int, int, int>(SafeCalculator.Add)
+                .Bind(SafeCalculator.AddOne)
+                .Apply(Result.Success(1));
+
+            var result = addTwo(Result.Success(1)).ItemOrDefault;
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
